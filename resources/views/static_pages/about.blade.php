c80e5ba7c43885644f51d8ef1a2b4408923e0c18
eric0927 <a0927511568@gmail.com>
2019-06-21 23:55:45 +0800

    基礎頁面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '關於頁')
+@section('content')
+  <h1>關於頁</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '幫助頁')
+@section('content')
+  <h1>幫助頁</h1>
+@stop
