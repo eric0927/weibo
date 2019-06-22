eacb5b2bfca5ddf946d22ac79f3ae7d9e0659a78
eric0927 <a0927511568@gmail.com>
2019-06-22 11:18:51 +0800

    初始化樣式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-  <h1>主頁</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      你現在看到的是 <a href="https://learnku.com/courses/laravel-ennential-training">Laravel 入門教程</a>的示例項目
+    </p>
+    <p>
+      一切，將從這裡開始
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="javascript:void(0);" role="button">現在註冊</a>
+    </p>
+  </div>
 @stop
