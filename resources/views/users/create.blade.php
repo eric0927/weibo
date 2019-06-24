faf8e7871cfc50937255fbaed04287e798e6c3c3
eric0927 <a0927511568@gmail.com>
2019-06-24 20:11:40 +0800

    用鳥注冊表單

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,41 @@
 @section('title', '注冊')
 
 @section('content')
-  <h1>註冊</h1>
+<div class="offset-md-2 col-md-8">
+<div class="card">
+  <div class="card-haeader">
+    <h5>註冊</h5>
+  </div>{{-- card-haeader --}}
+
+  <div class="card-body">
+    <form action="POST" action="{{ route('users.store') }}">
+      <div class="form-group">
+        <label for="name">名稱：</label>
+        <input type="text" name="name"
+                id="name" value="{{ old('name') }}" class="form-control" />
+      </div>            
+
+      <div class="form-group">
+        <label for="email">郵箱：</label>
+      <input type="text" name='email'
+              id="email" value="{{ old('email') }}" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="password">密碼：</label>
+      <input type="password" name="password"
+              id="password" value="{{ old('password') }}" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="password_confirmation">確認密碼</label>
+      <input type="password" name="password_confirmation" id="password_confirmation"
+              value="{{ old('password_confirmation') }}" class="form-control" />
+      </div>
+
+      <button type="submit" class="btn btn-primary">註冊</button>
+    </form>  
+  </div>{{-- card-body --}}  
+</div>{{-- card --}}  
+</div>{{-- offset-md2 col-md-8 --}}
 @stop
